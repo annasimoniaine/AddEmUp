bd079a91d7362f2061cb1812eaeb47d2e3bd8665
Simone Bakker <simonebakker94@gmail.com>
2018-01-04 17:59:39 +0100

    Working on camera, taking picture & displaying it works, converting to url and saving does not

--- app/src/main/java/com/simonebakker/simone/addemup/activities/AccountActivity.java
+++ app/src/main/java/com/simonebakker/simone/addemup/activities/AccountActivity.java
@@ -1,9 +1,14 @@
 package com.simonebakker.simone.addemup.activities;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -16,6 +21,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -27,15 +33,15 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.simonebakker.simone.addemup.Manifest;
 import com.simonebakker.simone.addemup.R;
-import com.simonebakker.simone.addemup.models.Game;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.io.ByteArrayOutputStream;
 
 public class AccountActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     private FirebaseUser mUser;
 
@@ -47,6 +53,7 @@ public class AccountActivity extends AppCompatActivity {
     private LinearLayout mNameLayout;
     private TextView mNameView;
     private EditText mNameEdit;
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -85,6 +92,18 @@ public class AccountActivity extends AppCompatActivity {
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // called when the camera is used to take a new account picture in editImage
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+//            mImageView.setImageBitmap(imageBitmap);
+
+            saveAccountImage(imageBitmap);
         }
     }
 
@@ -139,15 +158,13 @@ public class AccountActivity extends AppCompatActivity {
     }
 
     private void bindImage() {
-        ImageView imageView = findViewById(R.id.user_img);
+        mImageView = findViewById(R.id.user_img);
         Picasso.with(this)
                 .load(mPhotoUrl).placeholder(R.drawable.default_avatar).error(R.drawable.default_avatar)
                 .fit()
                 .centerInside()
-                .into(imageView);
-        imageView.setImageURI(mPhotoUrl);
-
-        // TODO: make clickable and use camera/files phone to add new image
+                .into(mImageView);
+        mImageView.setImageURI(mPhotoUrl);
     }
 
     private void setOnClicks() {
@@ -157,7 +174,7 @@ public class AccountActivity extends AppCompatActivity {
             public void onClick(View view) {
                 mNameLayout.setVisibility(View.GONE);
                 mEditNameLayout.setVisibility(View.VISIBLE);
-                mNameEdit.setHint(mName);
+                mNameEdit.setText(mName);
                 mNameEdit.setEnabled(true);
             }
         });
@@ -169,6 +186,7 @@ public class AccountActivity extends AppCompatActivity {
                 saveName();
             }
         });
+        setEditImage();
     }
 
     private void saveName() {
@@ -190,6 +208,63 @@ public class AccountActivity extends AppCompatActivity {
                         }
                     }
                 });
+    }
+
+    private void setEditImage() {
+        // check whether the device has a camera function
+        ImageView editImage = findViewById(R.id.edit_image);
+        PackageManager packageManager = AccountActivity.this.getPackageManager();
+        if (packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            editImage.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    takePicture();
+                }
+            });
+        } else {
+            editImage.setVisibility(View.GONE);
+        }
+    }
+
+    private void takePicture() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    private void saveAccountImage(Bitmap imageBitmap) {
+        final Uri NEW_URL = convertBitmapToUrl(imageBitmap);
+//        final Uri NEW_URL = Uri.parse("http://cimg.tvgcdn.net/i/2017/07/19/8da32aef-513d-46c7-9497-8bf58e18b22e/9d69eb37ae4443d6915957ee46e565fe/170718-sarah-hyland-shadowhunters.png");
+
+        UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                .setPhotoUri(NEW_URL)
+                .build();
+
+        mUser.updateProfile(profileUpdates)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            mPhotoUrl = NEW_URL;
+                            bindImage();
+                        }
+                    }
+                });
+
+    }
+
+    private Uri convertBitmapToUrl(Bitmap imageBitmap) {
+//        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+//        imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        ContentResolver contentResolver = this.getContentResolver();
+        String path = MediaStore.Images.Media.insertImage(contentResolver, imageBitmap, "Title", null);
+        if (path == null) {
+            Toast.makeText(this, "image failed to be stored", Toast.LENGTH_LONG).show();
+            return Uri.parse("http://images-production.global.ssl.fastly.net/uploads/posts/off_site_promo_image/127357/sarah-hyland-plastic-surgery.jpg");
+        } else {
+            return Uri.parse(path);
+        }
     }
 
     private void getStats() {
